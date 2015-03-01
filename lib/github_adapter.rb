c8b16472cd6343e23e4dbd10fe08c04e96b961b8
rvillage <rvillage@gmail.com>
2015-03-01 16:28:48 +0900

    Refactoring: add body_builder

--- lib/github_adapter.rb
+++ lib/github_adapter.rb
@@ -1,5 +1,5 @@
 require 'octokit'
-require 'erb'
+require './lib/github_adapter/body_builder'
 
 module HubotSlack
   Octokit.configure do |config|
@@ -9,11 +9,11 @@ module HubotSlack
   class GithubAdapter
     class << self
       def create_pbi(repository, title)
-        Octokit.create_issue(prefixed_repo(repository), title, pbi_body, labels: 'PBI').html_url
+        Octokit.create_issue(prefixed_repo(repository), title, BodyBuilder.pbi, labels: 'PBI').html_url
       end
 
       def create_release_pr(repository, title)
-        res = Octokit.create_pull_request(prefixed_repo(repository), 'master', 'develop', title, release_pr_body)
+        res = Octokit.create_pull_request(prefixed_repo(repository), 'master', 'develop', title, BodyBuilder.release_pr)
         add_label(repository, res.number, 'リリースブランチ')
 
         res.html_url
@@ -23,18 +23,6 @@ module HubotSlack
 
       def prefixed_repo(repository)
         "rvillage/#{repository}"
-      end
-
-      def build_body(file_path)
-        ERB.new(File.read(file_path)).result
-      end
-
-      def pbi_body
-        build_body("#{__dir__}/github_adapter/pbi_body.text.erb")
-      end
-
-      def release_pr_body
-        build_body("#{__dir__}/github_adapter/release_pr_body.text.erb")
       end
 
       def add_label(repository, issue_number, label)
